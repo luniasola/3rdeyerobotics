fda1fbf702085b29f8d8b6ddb8bfa31b6c585272
Julian Rulliansyah <jrulliansyah@gmail.com>
2024-12-13 11:52:01 +0700

    chore: configure ESLint with the added @stylistic/eslint-plugin rules for formatting consistent syntax, set up import order alignment rules, and integrate specific ESLint rules for TypeScript and Tailwind CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from 'next'
-import { Geist } 		 from 'next/font/google'
+import { Geist }         from 'next/font/google'
 
 import '@/app/assets/style/tailwind.css'
 
 const fontGeist = Geist({ variable : '--font-geist-sans', subsets : [ 'latin' ] })
 
 export const metadata: Metadata = {
-	title	    : '3rd Eye Robotics',
-	description : '3rd Eye Robotics Description',
+    title  	    : '3rd Eye Robotics',
+    description : '3rd Eye Robotics Description',
 }
 
-export default function RootLayout({ children }: Readonly<{children: React.ReactNode }>) {
-	return (
-		<html lang='en'>
-			<body className={fontGeist.variable + 'antialiased'}>
-				{children}
-			</body>
-		</html>
-	)
+export default function RootLayout({ children }: Readonly<{children : React.ReactNode }>) {
+    return (
+        <html lang={'en'}>
+            <body className={fontGeist.variable + 'antialiased'}>
+                {children}
+            </body>
+        </html>
+    )
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,71 @@
-import { dirname } 	     from 'path'
+import { dirname }       from 'path'
 import { fileURLToPath } from 'url'
 
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat }         from '@eslint/eslintrc'
+import pluginStylistic        from '@stylistic/eslint-plugin'
+import pluginAlignAssignments from 'eslint-plugin-align-assignments'
+import pluginAlignImport      from 'eslint-plugin-align-import'
+import pluginImport           from 'eslint-plugin-import'
+import pluginTailwind         from 'eslint-plugin-tailwindcss'
+import typescript             from 'typescript-eslint'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname  = dirname(__filename)
-const compat 	 = new FlatCompat({ baseDirectory: __dirname })
+const compat     = new FlatCompat({ baseDirectory: __dirname })
 
 const config = [ ...compat.extends('next/core-web-vitals', 'next/typescript') ]
 
-export default config
+export default typescript.config({ ignores: [ 'vendor' ], }, {
+    extends : [ ...config, ...typescript.configs['recommended'], ...pluginTailwind.configs['flat/recommended'] ],
+    files   : [ '**/*.{ts,tsx}' ],
+    plugins : {
+        '@stylistic'        : pluginStylistic,
+        'align-import'      : pluginAlignImport,
+        'align-assignments' : pluginAlignAssignments,
+        'import'            : pluginImport,
+        'tailwindcss'       : pluginTailwind,
+    },
+    rules: {
+        '@stylistic/array-bracket-spacing'           : [ 'error', 'always' ],
+        '@stylistic/comma-dangle'                    : [ 'error', 'always-multiline' ],
+        '@stylistic/indent'                          : [ 'error', 4 ],
+        '@stylistic/jsx-closing-tag-location'        : [ 'error' ],
+        '@stylistic/jsx-curly-brace-presence'        : [ 'error', { 'props': 'always', 'propElementValues': 'always', 'children': 'never' } ],
+        '@stylistic/jsx-curly-spacing'               : [ 'error', { 'when': 'never', 'children': true } ],
+        '@stylistic/jsx-pascal-case'                 : [ 'error', { 'allowAllCaps': true } ],
+        '@stylistic/jsx-props-no-multi-spaces'       : [ 'error' ],
+        '@stylistic/jsx-sort-props'                  : [ 'error', { 'shorthandLast': true, 'callbacksLast': true } ],
+        '@stylistic/jsx-tag-spacing'                 : [ 'error', { 'beforeSelfClosing': 'always', 'beforeClosing': 'never' } ],
+        '@stylistic/key-spacing'                     : [ 'error', { 'beforeColon': true, 'afterColon': true, 'align': 'colon' } ],
+        '@stylistic/no-mixed-spaces-and-tabs'        : [ 'error' ],
+        '@stylistic/no-multiple-empty-lines'         : [ 'error', { 'max': 1, 'maxBOF': 0, 'maxEOF': 0 } ],
+        '@stylistic/no-whitespace-before-property'   : [ 'error' ],
+        '@stylistic/padding-line-between-statements' : [ 'error', { 'blankLine': 'always', 'prev': '*', 'next': 'return' } ],
+        '@stylistic/quotes'                          : [ 'error', 'single' ],
+        '@stylistic/semi'                            : [ 'error', 'never' ],
+        '@typescript-eslint/no-unused-expressions'   : [ 'off' ],
+        'align-import/align-import'                  : [ 'error' ],
+        'eol-last'                                   : [ 'error', 'always' ],
+        'import/order'                               : [ 'error', {
+            'alphabetize' : { 'order': 'asc', 'caseInsensitive': true },
+            'groups'      : [ 'builtin', 'external', 'internal' ],
+            'pathGroups'  : [ 
+                { 'pattern' : 'next/*', 'group' : 'external', 'position' : 'before' }, 
+                { 'pattern' : 'react', 'group' : 'external', 'position' : 'before' },
+                { 'pattern' : '$/**',  'group' : 'internal', 'position' : 'before' },
+                { 'pattern' : '@/**',  'group' : 'internal', 'position' : 'after' } 
+            ],
+            'pathGroupsExcludedImportTypes' : [ 'next', 'react' ],
+            'newlines-between'              : 'always'
+        }],
+        'no-alert'          : [ 'error' ],
+        'no-console'        : [ 'error', { 'allow': [ 'error' ] } ],
+        'no-useless-concat' : [ 'off' ],
+        'prefer-template'   : [ 'off' ]
+    },
+    settings : {
+        tailwindcss : {
+            callees : [ 'classNames', 'cva' ]
+        },
+    },
+})
